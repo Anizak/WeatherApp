a8958c4e6460349099a7dd0aa4173b983e2f5673
Ani Zakaryan <zakaryan.ani.2001@gmail.com>
2025-03-06 23:04:00 +0400

    created function for generating weekdays

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -1,85 +1,108 @@
-import sun from "../assets/images/sun.png"
-import cloudy from "../assets/images/partly-cloudy.png"
-import fog from "../assets/images/haze.png"
-import drizzle from "../assets/images/drizzle.png"
-import rain from "../assets/images/rain.png"
-import snow from "../assets/images/snowfall.png"
-import thunder from "../assets/images/storm.png"
-import snowgrains from "../assets/images/haze.png"
-import night  from "../assets/images/night.png"
-import cloudyNight from "../assets/images/cloudy-night.png"
+import sun from "../assets/images/sun.png";
+import cloudy from "../assets/images/partly-cloudy.png";
+import fog from "../assets/images/haze.png";
+import drizzle from "../assets/images/drizzle.png";
+import rain from "../assets/images/rain.png";
+import snow from "../assets/images/snowfall.png";
+import thunder from "../assets/images/storm.png";
+import snowgrains from "../assets/images/haze.png";
+import night from "../assets/images/night.png";
+import cloudyNight from "../assets/images/cloudy-night.png";
 
-export const weatherTypes : Array<any> =[
-    {
-    id : 1,
-    name : "Clear sky",
+export const weatherTypes: Array<any> = [
+  {
+    id: 1,
+    name: "Clear sky",
     shortName: "Sunny",
-    icon : sun, 
-    nightIcon : night,
-    weatherCodes : [0],
-},
-    {
-    id : 2,
-    name : "Mainly clear, partly cloudy",
+    icon: sun,
+    nightIcon: night,
+    weatherCodes: [0],
+  },
+  {
+    id: 2,
+    name: "Mainly clear, partly cloudy",
     shortName: "Cloudy",
-    icon : cloudy,
+    icon: cloudy,
     nightIcon: cloudyNight,
-    weatherCodes : [1, 2, 3]
-},
-    {
-    id : 3,
-    name : "Fog and depositing rime fog",
+    weatherCodes: [1, 2, 3],
+  },
+  {
+    id: 3,
+    name: "Fog and depositing rime fog",
     shortName: "Fog",
-    icon : fog,
+    icon: fog,
     nightIcon: null,
-    weatherCodes : [45, 48]
-},
-    {
-    id : 4,
-    name : "Drizzle: Light, moderate, and dense intensity",
+    weatherCodes: [45, 48],
+  },
+  {
+    id: 4,
+    name: "Drizzle: Light, moderate, and dense intensity",
     shortName: "Drizzle",
-    icon : drizzle,
+    icon: drizzle,
     nightIcon: null,
-    weatherCodes : [51, 53, 55, 56, 57]
-},
-    {
-    id : 5,
-    name : "Rain: Slight, moderate and heavy intensity",
+    weatherCodes: [51, 53, 55, 56, 57],
+  },
+  {
+    id: 5,
+    name: "Rain: Slight, moderate and heavy intensity",
     shortName: "Rain",
-    icon : rain,
+    icon: rain,
     nightIcon: null,
-    weatherCodes : [61, 63, 65, 66, 67,80, 81, 82]
-},
-    {
-    id : 6,
-    name : "Snow fall: Slight, moderate, and heavy intensity",
+    weatherCodes: [61, 63, 65, 66, 67, 80, 81, 82],
+  },
+  {
+    id: 6,
+    name: "Snow fall: Slight, moderate, and heavy intensity",
     shortName: "Snow",
-    icon : snow,
+    icon: snow,
     nightIcon: null,
-    weatherCodes : [71, 73, 75,85, 86]
-},
-  
-    {
-    id : 7,
-    name : "Thunderstorm: Slight or moderate",
+    weatherCodes: [71, 73, 75, 85, 86],
+  },
+
+  {
+    id: 7,
+    name: "Thunderstorm: Slight or moderate",
     shortName: "Thunderstorm",
-    icon : thunder,
+    icon: thunder,
     nightIcon: null,
-    weatherCodes : [95, 96, 99]
-},
-    {
-    id : 8,
-    name : "Snow grains",
+    weatherCodes: [95, 96, 99],
+  },
+  {
+    id: 8,
+    name: "Snow grains",
     shortName: "Snow grains",
-    icon : snowgrains,
+    icon: snowgrains,
     nightIcon: null,
-    weatherCodes : [77]
-},
-]
+    weatherCodes: [77],
+  },
+];
 
+export const addressTypes: Array<string> = [
+  "city",
+  "country",
+  "village",
+  "town",
+  "road",
+  "suburb",
+  "state",
+  "street",
+];
 
-export const addressTypes : Array<string> = ["city", "country", "village", "town", "road", "suburb", "state", "street"];
+export const times: Array<string> = [
+  "06:00",
+  "09:00",
+  "12:00",
+  "15:00",
+  "18:00",
+  "21:00",
+];
 
-
-export const times : Array<string> = ["06:00", "09:00", "12:00", "15:00", "18:00", "21:00"];
-
+export const weekdays : any = {
+  0: "San",
+  1: "Man",
+  2: "Tue",
+  3: "Wed",
+  4: "Thu",
+  5: "Fri",
+  6: "Sat",
+};
--- src/services/functions.tsx
+++ src/services/functions.tsx
@@ -1,4 +1,4 @@
-import { weatherTypes } from "./data.service";
+import { weatherTypes, weekdays } from "./data.service";
 
 export const getWeatherType = (wcode: number) => {
   return weatherTypes.find((obj) => obj.weatherCodes.includes(wcode));
@@ -21,17 +21,37 @@ export const weatherDataWithTimes = (
   times: any,
   isDay: any,
   temperatures: any,
-  weatherCodes: any,
+  weatherCodes: any
 ) => {
-  const mergedArrays = times.map((elem: any, i: any) => ({time: elem.time, isDay: isDay[i], temperature: temperatures[i], weatherCode: weatherCodes[i]}));
+  const mergedArrays = times.map((elem: any, i: any) => ({
+    time: elem.time,
+    isDay: isDay[i],
+    temperature: temperatures[i],
+    weatherCode: weatherCodes[i],
+  }));
   return mergedArrays;
-  
 };
 
+export const generateWeekDay = (date: string) => {
+  //"2025-03-06T00:00"
+  const weekData = [];
+  const currentDay = new Date(date.split("T")[0]); //Wed Mar 05 2025 21:30:00 GMT+0400 (Armenia Standard Time)
 
-export const generateWeekDay = (date : string) =>{
-    const currentDay = new Date(date);
-    const currentWeekDay = currentDay.getDay();
-    console.log(currentDay,currentWeekDay);
-    
-}
+  for (let i = 1; i <= 7; i++) {
+    const date = new Date(currentDay.setDate(currentDay.getDate() + 1));
+    const year = date.getFullYear();
+    const month =
+      date.getMonth() + 1 < 10
+        ? `0${date.getMonth() + 1}`
+        : date.getMonth() + 1;
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+    let obj = {
+      weekday: weekdays[date.getDay()],
+      day: `${year}-${month}-${day}T15:00}`,
+      night: `${year}-${month}-${day}T03:00}`,
+    };
+
+    weekData.push(obj);
+  }
+  console.log(weekData);
+};
