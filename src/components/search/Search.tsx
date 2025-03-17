9ff04214a5d8e472f8990b0fb458559bda2b93e3
Ani Zakaryan <zakaryan.ani.2001@gmail.com>
2025-03-17 20:15:14 +0400

    added the home page

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -4,8 +4,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { getLocation } from "../../store/slices/weatherSlice/weatherApi";
 import { AppDispatch } from "../../store/store";
 import { selectWeather } from "../../store/slices/weatherSlice/weatherSlice";
+import { CiSearch } from "react-icons/ci";
 
-const Search = () => {
+
+const Search = ({isTransparent}: any) => {
   const [value, setValue] = useState<string>("");
   const dispatch = useDispatch<AppDispatch>();
   const { currentLocationSearch } = useSelector(selectWeather);
@@ -24,11 +26,15 @@ const Search = () => {
   return (
     <div className={styles.searchBox}>
       <form onSubmit={search}>
+      <CiSearch className={styles.searchIcon}/>
         <input
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
+          placeholder="Check the weather in your area"
+          className={isTransparent ? `${styles.searchField} ${styles.transparent}` : styles.searchField}
         />
+
       </form>
     </div>
   );
--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -7,25 +7,44 @@ import CurrentWeather from "../currentWeather/CurrentWeather";
 import TodaysForecast from "../todaysForcast/TodaysForecast";
 import AirConditions from "../airConditions/AirConditions";
 import WeekForecast from "../weekForecast/WeekForecast";
-import { generateWeekDay } from "../../services/functions";
+import skyVideo from "../../assets/videos/skyBackground.mp4";
 
 const Weather = () => {
   const { currentLocationSearch } = useSelector(selectWeather);
-  generateWeekDay("2025-03-05T21:30");
 
   return (
     <div className={styles.container}>
-      <div className={styles.leftSide}>
-        <Search />
-        <div className={styles.searchItemBox}>
-          {Object.keys(currentLocationSearch).length > 0 && <CurrentWeather />}
-          {Object.keys(currentLocationSearch).length > 0 && <TodaysForecast />}
-        </div>
-        <div className={styles.airConditionsBox}>
-          {Object.keys(currentLocationSearch).length > 0 && <AirConditions />}
-        </div>
-      </div>
-      <div className={styles.rightSide}>{Object.keys(currentLocationSearch).length > 0 && <WeekForecast />}</div>
+      {Object.keys(currentLocationSearch).length > 0 ? (
+        <>
+          <div className={styles.leftSide}>
+            <Search isTransparent={false} />
+            <div className={styles.searchItemBox}>
+              <CurrentWeather />
+              <TodaysForecast />
+            </div>
+            <div className={styles.airConditionsBox}>
+              <AirConditions />
+            </div>
+          </div>
+
+          <div className={styles.rightSide}>
+            <WeekForecast />
+          </div>
+        </>
+      ) : (
+        <>
+          <video autoPlay loop muted className={styles.video}>
+            <source src={skyVideo}></source>
+          </video>
+          <div className={styles.searchBox}>
+            <div className={styles.searchField}>
+              <Search isTransparent={true} />
+              <h1>Welcome to our Weather Forecast App</h1>
+              <p>Stay one step ahead of the weather!</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
